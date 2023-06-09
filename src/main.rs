8beb9a0d75fca76d4a9a91e2c6c26be4c0f2983b
Jason Eveleth <jj11@eveleth.com>
2023-06-09 19:46:01 -0400

    Can draw A

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,23 @@
-use std::ptr;
+use glium::Surface;
+use glium::implement_vertex;
+use glium::uniform;
+use glutin::dpi::LogicalSize;
+use rusttype::Font; 
+use rusttype::Scale; 
+use rusttype::point;
 
-use gl::types::{GLint, GLuint, GLsizeiptr, GLsizei, GLvoid, GLenum, GLchar};
-use glutin::{event_loop::{EventLoop, ControlFlow}, window::WindowBuilder, ContextBuilder, GlRequest, Api, event::{Event, WindowEvent}};
-
-use rusttype::{Font, Scale, point};
-
-const VERTEX_SHADER_SOURCE: &str = r#"
-#version 330 core
-
-layout (location = 0) in vec2 position;
-layout (location = 1) in vec2 tex_coords;
-
-out vec2 frag_tex_coords;
-
-void main()
-{
-    gl_Position = vec4(position, 0.0, 1.0);
-    frag_tex_coords = tex_coords;
-}
-"#;
-
-const FRAGMENT_SHADER_SOURCE: &str = r#"
-#version 330 core
-
-in vec2 frag_tex_coords;
-uniform sampler2D tex;
-out vec4 frag_color;
-
-void main()
-{
-    frag_color = texture(tex, frag_tex_coords);
-}
-"#;
-
-fn check_errors() {
-    unsafe {
-        loop {
-            let error = gl::GetError();
-            if error != gl::NO_ERROR {
-                println!("OpenGL error: {}", error);
-            } else {
-                break;
-            }
-        }
-    }
-}
-
-fn compile_shader(source: &str, shader_type: GLenum) -> GLuint {
-    unsafe {
-        let shader = gl::CreateShader(shader_type);
-        gl::ShaderSource(shader, 1, &(source.as_ptr() as *const _), &(source.len() as GLint));
-        gl::CompileShader(shader);
-
-        check_errors();
-        // CHECK ERRS
-        let mut success: GLint = 0;
-        gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut success);
-        if success == 1 {
-            ()
-        } else {
-            let mut error_log_size: GLint = 0;
-            gl::GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut error_log_size);
-            let mut error_log: Vec<u8> = Vec::with_capacity(error_log_size as usize);
-            gl::GetShaderInfoLog(
-                shader,
-                error_log_size,
-                &mut error_log_size,
-                error_log.as_mut_ptr() as *mut _,
-            );
-
-            error_log.set_len(error_log_size as usize);
-            let log = String::from_utf8(error_log).expect("failure");
-            eprintln!("{}", log);
-        }
-        // CHECK ERRS
-
-        shader
-    }
-}
-
-fn link_program(vertex_shader: GLuint, fragment_shader: GLuint) -> GLuint {
-    unsafe {
-        let program = gl::CreateProgram();
-        gl::AttachShader(program, vertex_shader);
-        gl::AttachShader(program, fragment_shader);
-        gl::LinkProgram(program);
-
-        check_errors();
-
-        gl::DeleteShader(vertex_shader);
-        gl::DeleteShader(fragment_shader);
-
-        program
-    }
-}
+use glium::{Texture2d, Program, VertexBuffer, IndexBuffer};
+use glium::index::PrimitiveType;
+use std::borrow::Cow;
 
 fn terminal_render(width: usize, height: usize, buffer: &[u8]) {
-    for y in 0..height {
+    for y in (0..height).rev() {
         for x in 0..width {
-            let char = [" ", ":", "|", "O", "W"][(buffer[y * width + x] / 52) as usize];
+            let r = buffer[(y * width + x)*4];
+            let _g = buffer[(y * width + x)*4 + 1];
+            let _b = buffer[(y * width + x)*4 + 2];
+            let _a = buffer[(y * width + x)*4 + 3];
+            let char = [" ", ":", "|", "O", "W"][(r / 52) as usize];
             print!("{char}{char}");
         }
         print!("\n");
@@ -121,13 +40,20 @@ fn main() {
     let height = bbox.height() as usize;
 
     // create bitmap to store the glyph's pixel data
-    let mut buffer = vec![0u8; width * height];
+    let mut buffer = vec![0u8; width * height * 4]; // *4 for rgba
 
     glyph.draw(|x, y, v| {
         let x = x as usize;
-        let y = y as usize;
-        let index = y * width + x;
-        buffer[index] = (v * 255.0) as u8; // Store the alpha value in the buffer
+        let y = height - y as usize - 1; // flip y over
+        let index = (y * width + x) * 4;
+
+        let v = (v * 255.0) as u8;
+        let (r, g, b, a) = (v, v, v, 1);
+
+        buffer[index] = r;
+        buffer[index + 1] = g;
+        buffer[index + 2] = b;
+        buffer[index + 3] = a;
     });
 
     terminal_render(width, height, &buffer);
@@ -135,126 +61,93 @@ fn main() {
     run(width, height, buffer);
 }
 
-fn run(width: usize, height: usize, bitmap: Vec<u8>) {
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().with_title("My Boy");
+const VERTEX_SHADER_SOURCE: &str = r#"
+    #version 330 core
 
-    let gl_context = ContextBuilder::new()
-        .with_gl(GlRequest::Specific(Api::OpenGl, (3, 3)))
-        .build_windowed(window, &event_loop)
-        .expect("Cannot create windowed context");
+    in vec2 position;
+    in vec2 tex_coords;
+    out vec2 v_tex_coords;
 
-    let gl_context = unsafe {
-        gl_context
-            .make_current()
-            .expect("Failed to make context current")
-    };
-    gl::load_with(|ptr| gl_context.get_proc_address(ptr) as *const _);
+    void main() {
+        gl_Position = vec4(position, 0.0, 1.0);
+        v_tex_coords = tex_coords;
+    }
+"#;
 
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
+const FRAGMENT_SHADER_SOURCE: &str = r#"
+    #version 330 core
 
-        let vertex_shader = compile_shader(VERTEX_SHADER_SOURCE, gl::VERTEX_SHADER);
-        let fragment_shader = compile_shader(FRAGMENT_SHADER_SOURCE, gl::FRAGMENT_SHADER);
-        let shader_program = link_program(vertex_shader, fragment_shader);
+    uniform sampler2D tex;
+    in vec2 v_tex_coords;
+    out vec4 color;
 
-        match event {
-            Event::LoopDestroyed => (),
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                WindowEvent::Resized(physical_size) => gl_context.resize(physical_size),
+    void main() {
+        color = texture(tex, v_tex_coords);
+    }
+"#;
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+    tex_coords: [f32; 2],
+}
+
+implement_vertex!(Vertex, position, tex_coords);
+
+fn run(bitmap_width: usize, bitmap_height: usize, bitmap_data: Vec<u8>) {
+    let size = LogicalSize {width: 800, height: 600};
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new().with_inner_size(size).with_title("MY boi");
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    let raw_image = glium::texture::RawImage2d::from_raw_rgba(bitmap_data, (bitmap_width as u32, bitmap_height as u32));
+
+    // Create a texture from the bitmap data
+    let texture = Texture2d::new(&display, raw_image).unwrap();
+
+    // Compile the shaders and create the program
+    let program = Program::from_source(&display, VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE, None).unwrap();
+
+    // Create a vertex buffer for the quad
+    let vertex_buffer = VertexBuffer::new(&display, &[
+        Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0] },
+        Vertex { position: [-0.5,  0.5], tex_coords: [0.0, 1.0] },
+        Vertex { position: [ 0.5,  0.5], tex_coords: [1.0, 1.0] },
+        Vertex { position: [ 0.5, -0.5], tex_coords: [1.0, 0.0] },
+    ]).unwrap();
+
+    // Create an index buffer for the quad
+    let index_buffer = IndexBuffer::new(&display, PrimitiveType::TriangleStrip, &[1u32, 2u32, 0u32, 3u32]).unwrap();
+
+    event_loop.run(move |ev, _, control_flow| {
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 1.0);
+
+        // Bind the vertex buffer, index buffer, texture, and program
+        target.draw(
+            &vertex_buffer,
+            &index_buffer,
+            &program,
+            &uniform! { tex: &texture },
+            &Default::default(),
+        ).unwrap();
+
+        // Finish the frame
+        target.finish().unwrap();
+
+        let next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
                 _ => (),
             },
-            Event::RedrawRequested(_) => {
-                // set background color
-                unsafe {
-                    gl::ClearColor(0.0, 0.0, 1.0, 1.0);
-                    gl::Clear(gl::COLOR_BUFFER_BIT);
-                }
-
-                // generate an id for texture
-                let mut texture_id: GLuint = 0;
-                unsafe {
-                    gl::GenTextures(1, &mut texture_id);
-                    gl::BindTexture(gl::TEXTURE_2D, texture_id);
-                    // Set texture parameters (optional)
-                    gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as GLint);
-                    gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as GLint);
-                    gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as GLint);
-                    gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as GLint);
-
-                    // put bitmap on texture
-                    gl::TexImage2D(
-                        gl::TEXTURE_2D,
-                        0, // Mipmap level
-                        gl::RGBA as GLint, // Internal format
-                        width as GLint,
-                        height as GLint,
-                        0, // Border (always 0)
-                        gl::RGBA, // Format of the pixel data
-                        gl::UNSIGNED_BYTE, // Type of the pixel data
-                        bitmap.as_ptr() as *const GLvoid, // Pointer to the pixel data
-                    );
-                    gl::GenerateMipmap(gl::TEXTURE_2D);
-                }
-
-                // Create the vertex buffer object (VBO) and vertex array object (VAO) for the textured quad
-                let vertices: [f32; 16] = [
-                    // Positions      // Texture coordinates
-                    -0.5, -0.5,       0.0, 0.0,
-                    0.5, -0.5,        1.0, 0.0,
-                    0.5, 0.5,         1.0, 1.0,
-                    -0.5, 0.5,        0.0, 1.0,
-                ];
-
-                let mut vbo: GLuint = 0;
-                let mut vao: GLuint = 0;
-                unsafe {
-                    gl::GenBuffers(1, &mut vbo);
-                    gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-                    gl::BufferData(
-                        gl::ARRAY_BUFFER,
-                        (vertices.len() * std::mem::size_of::<f32>()) as GLsizeiptr,
-                        vertices.as_ptr() as *const GLvoid,
-                        gl::STATIC_DRAW,
-                    );
-
-                    gl::GenVertexArrays(1, &mut vao);
-                    gl::BindVertexArray(vao);
-
-                    // Set the vertex attribute pointers
-                    let stride = (4 * std::mem::size_of::<f32>()) as GLsizei;
-                    gl::VertexAttribPointer(0, 2, gl::FLOAT, gl::FALSE, stride, ptr::null());
-                    gl::EnableVertexAttribArray(0);
-                    gl::VertexAttribPointer(1, 2, gl::FLOAT, gl::FALSE, stride, (2 * std::mem::size_of::<f32>()) as *const GLvoid);
-                    gl::EnableVertexAttribArray(1);
-
-                    // gl::BindVertexArray(0);
-                    // gl::BindBuffer(gl::ARRAY_BUFFER, 0);
-                }
-
-                // Bind and activate the texture
-                unsafe {
-                    gl::UseProgram(shader_program);
-                    gl::BindVertexArray(vao);
-
-                    // Set the texture uniform
-                    let tex_uniform_location = gl::GetUniformLocation(shader_program, "tex\0".as_ptr() as *const GLchar);
-                    gl::Uniform1i(tex_uniform_location, 0); // Texture unit 0
-
-                    gl::ActiveTexture(gl::TEXTURE0);
-                    gl::BindTexture(gl::TEXTURE_2D, texture_id);
-
-                    // Render the quad
-                    gl::DrawArrays(gl::TRIANGLE_FAN, 0, 4);
-
-                    gl::BindVertexArray(0);
-                    gl::UseProgram(0);
-                }
-
-                gl_context.swap_buffers().unwrap();
-            }
             _ => (),
         }
     });
+
 }
