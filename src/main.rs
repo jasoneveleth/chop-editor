47ba5af88fd96b8ac2c7f6098498083b2fdece2a
Jason Eveleth <jj11@eveleth.com>
2023-06-11 02:21:20 -0400

    font stuff and scrolling!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use rusttype::Font; 
-use glutin::dpi::LogicalSize;
-use glutin::event::WindowEvent::CloseRequested;
-use glutin::event::WindowEvent::Resized;
-use glutin::event::Event;
+use glutin::dpi::{LogicalSize, PhysicalPosition};
+use glutin::event::WindowEvent::{CloseRequested, MouseWheel};
+use glutin::event::{Event, MouseScrollDelta};
 use log::info;
 
 // use pager::render::terminal_render;
@@ -14,7 +13,7 @@ fn main() {
 
     let font_data = include_bytes!("/Users/jason/Library/Fonts/Hack-Regular.ttf");
     let font = Font::try_from_bytes(font_data).expect("Error loading font");
-    let size = 250.0;
+    let size = 150.0;
     let atlas = GlyphAtlas::from_font(&font, size);
 
     // terminal_render(atlas.width, atlas.height, &atlas.buffer);
@@ -32,6 +31,7 @@ fn run(glyph_atlas: GlyphAtlas, font: Font<'static>, font_size: f32) {
     let window = cb.build_windowed(wb, &event_loop).unwrap();
 
     let display = Display::new(glyph_atlas, window);
+    let mut scroll_y = 0.0;
 
     event_loop.run(move |ev, _, control_flow| {
         let next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667); // 1/60 of a second
@@ -43,12 +43,48 @@ fn run(glyph_atlas: GlyphAtlas, font: Font<'static>, font_size: f32) {
                     *control_flow = glutin::event_loop::ControlFlow::Exit;
                     return;
                 },
+                MouseWheel{delta, ..} => {
+                    match delta {
+                        MouseScrollDelta::LineDelta(_, y) => {
+                            // Adjust the scroll position based on the scroll delta
+                            scroll_y += y * 20.0; // Adjust the scroll speed as needed
+                        },
+                        MouseScrollDelta::PixelDelta(PhysicalPosition{x: _, y}) => {
+                            scroll_y += y as f32;
+
+                            // BAD BAD copying code (should just redraw)
+                            let scale = rusttype::Scale::uniform(font_size);
+                            let line_num = 0;
+                            let (verts1, triangles1) = display.add_text(&font, scale, &format!("yeehaw"), line_num, 0, scroll_y);
+                            let (verts2, triangles2) = display.add_text(&font, scale, &format!("next line"), 1, verts1.len(), scroll_y);
+
+                            let mut verts = Vec::new();
+                            verts.extend(verts1);
+                            verts.extend(verts2);
+
+                            let mut triangles = Vec::new();
+                            triangles.extend(triangles1);
+                            triangles.extend(triangles2);
+                            display.draw(verts, triangles);
+                        },
+                    }
+                },
                 _ => (),
             },
             Event::RedrawRequested(_window_id) => {
                 info!("redraw requested");
                 let scale = rusttype::Scale::uniform(font_size);
-                let (verts, triangles) = display.add_text(&font, scale, &format!("yeehaw"));
+                let line_num = 0;
+                let (verts1, triangles1) = display.add_text(&font, scale, &format!("yeehaw"), line_num, 0, scroll_y);
+                let (verts2, triangles2) = display.add_text(&font, scale, &format!("next line"), 1, verts1.len(), scroll_y);
+
+                let mut verts = Vec::new();
+                verts.extend(verts1);
+                verts.extend(verts2);
+
+                let mut triangles = Vec::new();
+                triangles.extend(triangles1);
+                triangles.extend(triangles2);
                 display.draw(verts, triangles);
             }
             _ => (),
--- src/render.rs
+++ src/render.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use log::error;
+use log::info;
 use log::warn;
 use rusttype::Scale; 
 use rusttype::point;
@@ -15,7 +17,6 @@ use std::ops::Add;
 
 use log::debug;
 
-use im::Vector;
 use term_size;
 
 fn calc_chunk_size(default_val: usize) -> usize {
@@ -115,25 +116,64 @@ fn dims2pos(width_pixels: usize, height_pixels: usize, position: rusttype::Rect<
     ]
 }
 
+fn char2hex(c: char) -> String {
+    let mut buf = [0u8; 4];
+    let bytes = c.encode_utf8(&mut buf);
+    bytes.bytes().fold(String::new(), |acc, byte| acc + &format!("\\x{:x}", byte))
+}
+
+// Function to check if a character is a variation selector
+fn is_variation_selector(c: &char) -> bool {
+    let variation_selector_range = '\u{FE00}'..='\u{FE0F}';
+    variation_selector_range.contains(c)
+}
+
 impl GlyphAtlas {
     pub fn from_font(font: &Font, font_size: f32) -> Self {
         let scale = Scale::uniform(font_size);
 
+        // let all_chars = "ab❤️";
         let all_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`1234567890-=~!@#$%^&*()_+[]\\{}|;':\",./<>?";
-        let all_glyphs: Vec<rusttype::PositionedGlyph> = all_chars.chars().map(|c| {
+
+        // we are skipping variation selectors for emojis since I'm not very smart
+        let all_glyphs: Vec<rusttype::PositionedGlyph> = all_chars.chars().filter(|x| !is_variation_selector(x)).map(|c| {
             let glyph = font.glyph(c);
+
+            // the glyph id for a glyph that isn't defined is 0
+            let notdef_id = rusttype::GlyphId(0);
+            let glyph = if glyph.id() == notdef_id {
+                warn!("`{c}` with bytes: {} not found in font!", char2hex(c));
+
+                // // this is slow, but finds the emojis (but we can't seem to use them, so...)
+                // let font_data = include_bytes!("/System/Library/Fonts/Apple Color Emoji.ttc");
+                // let font = Font::try_from_bytes(font_data).expect("Error loading font");
+                // let glyph = font.glyph(c);
+                // if glyph.id() == notdef_id {
+                //     warn!("fuck it isn't an emoji");
+                // }
+                glyph
+            } else {
+                glyph
+            };
+
             let glyph = glyph.scaled(scale);
             glyph.positioned(point(0.0, 0.0))
         }).collect();
 
-        let width_pixels = all_glyphs.iter().map(|g| {
-            let width = g.pixel_bounding_box().unwrap().width();
+        let width_pixels = all_glyphs.iter().enumerate().map(|(i, g)| {
+            let width = g.pixel_bounding_box().map(|x| x.width()).unwrap_or_else(|| {
+                let c = all_chars.chars().nth(i).unwrap_or('?');
+                error!("couldn't generate the bounding box for char (or ? if unwrap failed): `{}`, bytes: {}", c, char2hex(c));
+                0});
             width
         }).sum::<i32>() as usize;
         let width_pixels = width_pixels + all_chars.len();
 
-        let height_pixels = all_glyphs.iter().map(|g| {
-            let height = g.pixel_bounding_box().unwrap().height();
+        let height_pixels = all_glyphs.iter().enumerate().map(|(i, g)| {
+            let height = g.pixel_bounding_box().map(|x| x.height()).unwrap_or_else(|| {
+                let c = all_chars.chars().nth(i).unwrap_or('?');
+                error!("couldn't generate the bounding box for char (or ? if unwrap failed): `{}`, bytes: {}", c, char2hex(c));
+                0});
             height
         }).max().unwrap() as usize;
 
@@ -145,33 +185,36 @@ impl GlyphAtlas {
 
         let mut curr_x = 0;
         for (c, glyph) in all_chars.chars().zip(all_glyphs) {
-            let bbox = glyph.pixel_bounding_box().unwrap();
-            let glyph_width = bbox.width() as usize;
-            let glyph_height = bbox.height() as usize;
+            if let Some(bbox) = glyph.pixel_bounding_box() {
+                let glyph_width = bbox.width() as usize;
+                let glyph_height = bbox.height() as usize;
 
-            glyph.draw(|x, y, v| {
-                let x = curr_x + x as usize;
+                glyph.draw(|x, y, v| {
+                    let x = curr_x + x as usize;
 
-                let y = glyph_height - y as usize - 1; // flip y over
+                    let y = glyph_height - y as usize - 1; // flip y over
 
-                let index = (y * width_pixels + x) * 4;
+                    let index = (y * width_pixels + x) * 4;
 
-                let v = (v * 255.0) as u8;
+                    let v = (v * 255.0) as u8;
 
-                buffer[index] = 255;
-                buffer[index + 1] = 255;
-                buffer[index + 2] = 255;
-                buffer[index + 3] = v;
-            });
+                    buffer[index] = 255;
+                    buffer[index + 1] = 255;
+                    buffer[index + 2] = 255;
+                    buffer[index + 3] = v;
+                });
 
-            let top_left = rusttype::point(curr_x, height_pixels - glyph_height);
-            let bottom_right = rusttype::point(curr_x + glyph_width, height_pixels - 1);
-            let dims = rusttype::Rect { min: top_left, max: bottom_right};
+                let top_left = rusttype::point(curr_x, height_pixels - glyph_height);
+                let bottom_right = rusttype::point(curr_x + glyph_width, height_pixels - 1);
+                let dims = rusttype::Rect { min: top_left, max: bottom_right};
 
-            let tex_pos = dims2pos(width_pixels, height_pixels, dims);
-            map.insert(c, GlyphData{width: glyph_width, height: glyph_height, tex_pos});
+                let tex_pos = dims2pos(width_pixels, height_pixels, dims);
+                map.insert(c, GlyphData{width: glyph_width, height: glyph_height, tex_pos});
 
-            curr_x += glyph_width+1;
+                curr_x += glyph_width+1;
+            } else {
+                warn!("`{c}` with bytes: {} not found in font!", char2hex(c));
+            }
         }
 
         Self {buffer, width: width_pixels, height: height_pixels, map}
@@ -242,12 +285,30 @@ impl Display {
         Self {glyph_info: glyph_atlas.map, glium_display: display, program, texture}
     }
 
-    pub fn add_text(&self, font: &Font, scale: Scale, text: &str) -> (Vector<Vertex>, Vector<u32>) {
+    pub fn add_text(&self, font: &Font, scale: Scale, text: &str, line_num: usize, vertex_index_start: usize, scroll_y: f32) -> (Vec<Vertex>, Vec<u32>) {
         let window_size = self.glium_display.gl_window().window().inner_size();
 
         let v_metrics = font.v_metrics(scale);
-        let padding = 0.;
-        let glyphs: Vec<_> = font.layout(text, scale, point(padding, padding + v_metrics.ascent)).collect();
+        let line_height = v_metrics.ascent - v_metrics.descent;
+
+        // 0,0 is top left, positive y goes down
+        let start_x = 0.0;
+        let start_y = line_num as f32 * (line_height + v_metrics.line_gap) + scroll_y;
+
+        // check if line is off screen
+        let bottom_of_line = start_y - v_metrics.descent + line_height;
+        let top_of_line = start_y - v_metrics.ascent;
+        if  bottom_of_line < 0. || top_of_line > window_size.height as f32 {
+            return (Vec::new(), Vec::new())
+        }
+
+        // source: https://git.xobs.io/xobs/rust-font-test/src/branch/master/src/main.rs line 281
+        // The origin of a line of text is at the baseline (roughly where
+        // non-descending letters sit). We don't want to clip the text, so we shift
+        // it down with an offset when laying it out. v_metrics.ascent is the
+        // distance between the baseline and the highest edge of any glyph in
+        // the font. That's enough to guarantee that there's no clipping.
+        let glyphs: Vec<_> = font.layout(text, scale, point(start_x, start_y + v_metrics.ascent)).collect();
 
         // // work out the layout size of fully rendered text
         // let glyphs_height = (v_metrics.ascent - v_metrics.descent).ceil() as u32;
@@ -266,32 +327,41 @@ impl Display {
         let mut vertices = Vec::new();
         let mut indices: Vec<u32> = Vec::new();
 
-        for (glyph, c) in glyphs.iter().zip(text.chars()) {
-            let bbox = glyph.pixel_bounding_box().unwrap();
+        for (glyph, c) in glyphs.iter().zip(text.chars().filter(|x| !is_variation_selector(x))) {
+            // don't render any pixels for a space, we already know the spot the next one should go
+            if c == ' ' {
+                continue;
+            }
+
+            let bbox = glyph.pixel_bounding_box().expect(&format!("character `{c}` bytes: {}", char2hex(c)));
 
             let x = (bbox.min.x as f32 / window_size.width as f32) * 2.0;
             let y = ((window_size.height as f32 - bbox.min.y as f32) / window_size.height as f32) * 2.0;
             let top_left = rusttype::point(x-1., y-1.);
-
-            if top_left.x <= -1. {
-                warn!("top left of glyph is rendered off (left) screen")
-            }
-            if top_left.y <= -1. {
-                warn!("top left of glyph is rendered off (bottom) screen")
-            }
-            if top_left.x >= 1. {
-                warn!("top left of glyph is rendered off (right) screen")
-            }
-            if top_left.y >= 1. {
-                warn!("top left of glyph is rendered off (top) screen")
-            }
 
             let glyph_data = self.glyph_info.get(&c).unwrap();
 
             let new_width = (glyph_data.width as f32 / window_size.width as f32) * 2.0;
             let new_height = (glyph_data.height as f32 / window_size.height as f32) * 2.0;
 
-            let index = vertices.len();
+            if top_left.x + new_width <= -1. {
+                // warn!("glyph is rendered off (left) of screen")
+                continue;
+            }
+            if top_left.y <= -1. {
+                // warn!("glyph is rendered off (bottom) of screen")
+                continue;
+            }
+            if top_left.x >= 1. {
+                // warn!("glyph is rendered off (right) of screen")
+                continue;
+            }
+            if top_left.y - new_height >= 1. {
+                // warn!("glyph is rendered off (top) of screen")
+                continue;
+            }
+
+            let index = vertices.len() + vertex_index_start;
 
             vertices.push(Vertex { position: [top_left.x, top_left.y - new_height], tex_coords: glyph_data.tex_pos[0] });
             vertices.push(Vertex { position: [top_left.x, top_left.y], tex_coords: glyph_data.tex_pos[1] });
@@ -308,17 +378,15 @@ impl Display {
             indices.push((index + 2) as u32);
             indices.push((index + 3) as u32);
         }
-        (Vector::from(vertices), Vector::from(indices))
+        (vertices, indices)
     }
 
-    pub fn draw(&self, vertex_list: Vector<Vertex>, triangle_list: Vector<u32>) {
+    pub fn draw(&self, vertex_list: Vec<Vertex>, triangle_list: Vec<u32>) {
         // all the vertices we want to pass to the GPU
-        let arr: Vec<Vertex> = vertex_list.iter().copied().collect();
-        let vertex_buffer = VertexBuffer::new(&self.glium_display, &arr[..]).unwrap();
+        let vertex_buffer = VertexBuffer::new(&self.glium_display, &vertex_list[..]).unwrap();
 
         // a list of triangles of vertex indices
-        let arr: Vec<u32> = triangle_list.iter().copied().collect();
-        let index_buffer = IndexBuffer::new(&self.glium_display, PrimitiveType::TrianglesList, &arr[..]).unwrap();
+        let index_buffer = IndexBuffer::new(&self.glium_display, PrimitiveType::TrianglesList, &triangle_list[..]).unwrap();
 
         let mut target = self.glium_display.draw();
         target.clear_color(0.0, 0.0, 0.0, 1.0);
